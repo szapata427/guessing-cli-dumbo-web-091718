77dd990c2fa4b24cb869c100cf0375a68ee562d0
Steven Zapata <szapata427@gmail.com>
2018-09-14 02:47:12 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,16 +5,14 @@ guess = gets.chomp
 
 random_guess = rand(1..6)
 
-loop do
-  
-  if guess.to_i == random_guess
+while guess != "exit"
+
+if guess.to_i == random_guess
   puts "You guessed the correct number!"
   
+  else guess.to_i != random_guess
   
-  elsif guess.to_i != random_guess
-    puts "The computer guessed #{random_guess}"
-  
-  
+  puts "The computer guessed #{random_guess}"
   
     end
   end
