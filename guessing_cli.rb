8b091adac4dd9a0a9e89340e9b9eba902babd449
Steven Zapata <szapata427@gmail.com>
2018-09-14 02:51:56 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -12,10 +12,8 @@ if guess == random_guess
   puts "You guessed the correct number!"
   
   else 
-  
   puts "The computer guessed #{random_guess}"
-  
-    end
+     end
     
     puts "Guess a number between 1 and 6."
     random_guess = rand(1..6).to_s
